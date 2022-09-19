accc2f5708e510ee82851fea7d84035b88b4edd0
doc <klempaleks@mail.ru>
2022-09-19 13:14:12 +0900

    ДЗ Массивы Ч.1 Дмитрий, Ваши замечания учтены. Все исправлено!

--- src/HWArraysPartOne.java
+++ src/HWArraysPartOne.java
@@ -1,17 +1,20 @@
+import java.util.Arrays;
+
 public class HWArraysPartOne {
     public static void main(String[] args) {
 
         System.out.println();
         System.out.println("Массивы. Часть 1. Задача 1. Массив 1.");
-        int[] nums = new int[]{1, 2, 3};
+        int[] nums = new int[3];
         nums[0] = 1;
+        nums[1] = 2;
+        nums[2] = 3;
         for (int i = 0; i < nums.length; i++) {
             System.out.println(nums[i]);
         }
         System.out.println();
         System.out.println("Массивы. Часть 1. Задача 1. Массив 2.");
         double[] nums1 = {1.57, 7.654, 9.986};
-        nums1[0] = 1.57;
         for (int i = 0; i < nums1.length; i++) {
             System.out.println(nums1[i]);
         }
@@ -20,8 +23,7 @@ public class HWArraysPartOne {
         System.out.println("Массивы. Часть 1. Задача 1. Массив 3.");
         // Динамика уменьшения продолжительности дня в часах за неделю.
         double[] longDay = {12.48, 12.45, 12.41, 12.37, 12.33, 12.30, 12.26};
-        longDay[0] = 12.48;
-        for (int i = 0; i < 7; i++) {
+        for (int i = 0; i < longDay.length; i++) {
             System.out.println(longDay[i]);
         }
         System.out.println();
@@ -44,7 +46,7 @@ public class HWArraysPartOne {
             System.out.print(nums1[i] + ", ");
         }
         // массив 3
-        for (int i = 0; i < 7; i++) {
+        for (int i = 0; i < longDay.length; i++) {
             if (i == longDay.length - 1) {
                 System.out.println(longDay[i]);
                 break;
@@ -54,24 +56,24 @@ public class HWArraysPartOne {
         System.out.println("Массивы. Часть 1. Задача 3.");
 
 // массив 1
-        for (int i = 2; i < nums.length; i--) {
-            if (i == nums.length - 3) {
+        for (int i = nums.length - 1; i < nums.length; i--) {
+            if (i == 0) {
                 System.out.println(nums[i]);
                 break;
             }
             System.out.print(nums[i] + ", ");
         }
         // массив 2
-        for (int i = 2; i < nums1.length; i--) {
-            if (i == nums1.length - 3) {
+        for (int i = nums1.length - 1; i < nums1.length; i--) {
+            if (i == 0) {
                 System.out.println(nums1[i]);
                 break;
             }
             System.out.print(nums1[i] + ", ");
         }
         // массив 3
-        for (int i = 6; i < longDay.length; i--) {
-            if (i == longDay.length - 7) {
+        for (int i = longDay.length - 1; i < longDay.length; i--) {
+            if (i == 0) {
                 System.out.println(longDay[i]);
                 break;
             }
@@ -79,15 +81,12 @@ public class HWArraysPartOne {
         }
         System.out.println();
         System.out.println("Массивы. Часть 1. Задача 4.");
-        for (int i = 0; i < nums.length; i++)
+        for (int i = 0; i < nums.length; i++){
             if (nums[i] % 2 != 0){
-                nums[i] = nums[i]+ 1;
-                if (i == nums.length - 1) {
-                    System.out.println(nums[i]);
-                    break;
-                }
-                System.out.print(nums[i] + ", ");
-            }
+                nums[i] = nums[i] + 1;}}
+        System.out.println(Arrays.toString(nums));
+
+
 
     }
 }
